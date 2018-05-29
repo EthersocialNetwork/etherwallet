feb2f5d1e30200b9a53d212cc252fc58986d5da6
kimmyeonghun <kimmh@genercrypto.com>
2018-05-29 19:27:48 +0900

    TREZOR/Ledger support for EtherSocial Network (ESN)

--- app/scripts/controllers/decryptWalletCtrl.js
+++ app/scripts/controllers/decryptWalletCtrl.js
@@ -34,6 +34,7 @@ var decryptWalletCtrl = function($scope, $sce, walletService) {
         hwRskPath:         "m/44'/137'/0'/0",      // first address : m/44'/137'/0'/0/0
         goPath:            "m/44'/6060'/0'/0",     // first address: m/44'/6060'/0'/0/0
         hwEOSClassicPath:  "m/44'/2018'/0'/0",     // first address: m/44'/2018'/0'/0/0
+        hwESNetworkPath:   "m/44'/31102'/0'/0",    // first address: m/44'/31102'/0'/0/0
     };
     $scope.HDWallet.dPath = $scope.HDWallet.defaultDPath;
     $scope.mnemonicModel = new Modal(document.getElementById('mnemonicModel'));
@@ -112,6 +113,9 @@ var decryptWalletCtrl = function($scope, $sce, walletService) {
                 case nodes.nodeTypes.EOSC:
                     $scope.HDWallet.dPath = $scope.HDWallet.hwEOSClassicPath;
                     break;
+                case nodes.nodeTypes.ESN:
+                    $scope.HDWallet.dPath = $scope.HDWallet.hwESNetworkPath;
+                    break;
                 default:
                     $scope.HDWallet.dPath = $scope.HDWallet.trezorPath;
             }
@@ -152,6 +156,9 @@ var decryptWalletCtrl = function($scope, $sce, walletService) {
                     break;
                 case nodes.nodeTypes.EOSC:
                     $scope.HDWallet.dPath = $scope.HDWallet.hwEOSClassicPath;
+                    break;
+                case nodes.nodeTypes.ESN:
+                    $scope.HDWallet.dPath = $scope.HDWallet.hwESNetworkPath;
                     break;
                 default:
                   $scope.HDWallet.dPath = $scope.HDWallet.defaultDPath;
